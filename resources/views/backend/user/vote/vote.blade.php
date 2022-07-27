2199d1d86b60223659424c6df751de579b7c6226
YeaminRabbi <yeaminrabbi308@gmail.com>
2022-07-28 00:05:22 +0600

    changes in voting poll

--- resources/views/backend/user/vote/vote.blade.php
+++ resources/views/backend/user/vote/vote.blade.php
@@ -10,7 +10,7 @@
                 <div class="row">
                     @if ($candidates->isNotEmpty())
                         @foreach ($candidates as $data)
-                            <div class="col-md-3">
+                            <div class="col-md-4">
                                 <div class="card profile-card-2">
                                     <div class="card-img-block">
                                         <img class="img-fluid" src="{{ asset('images/candidate/'.$data->created_at->format('Y/M/').'/'.$data->image) }}" alt="Candidate Image">
@@ -65,18 +65,76 @@
                             <br>
                     @endforeach
                 @endif
-               
+
+                <hr>
+                            
+                    <h5>Choose your candidate</h5>
+                
+                    <div class="row">
+                            <div class="col">
+                                @if ($candidates->isNotEmpty())
+                                    @foreach ($candidates as $key => $data)
+                                    
+                                        <label for="voter-{{ $key }}">
+                                            <img onclick=selectCandidate('candidateName-{{ $key }}','voter-{{ $key }}') src="{{ asset('images/candidate/'.$data->created_at->format('Y/M/').'/'.$data->image) }}" alt="candidate img" style="border-radius: 50%; width:50px;height:50px;cursor: pointer;">
+                                        </label>    
+                                        <input type="hidden" value="{{ $data->name }}" id="candidateName-{{ $key }}">
+                                        <input type="hidden" name="voteMe" id="voter-{{ $key }}"  value="{{ $data->id }}">
+
+                                    @endforeach
+                                @endif
+
+                            </div>
+
+                            
+
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col">
+                            <h3 class="text-center" id="SelectedName"></h3>
+                            <form action="" method="POST" id="FORMSUBMIT" style="display: none;">
+                                @csrf
+                                <input type="hidden" value="" id="getcandidateID">
+                                <div class="row">
+                                    <div class="col-12">
+                                        <button type="submit" class="btn btn-success" style="margin-left: 40%;" onclick="return confirm('Confirm this candidate!?')">Submit</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
             </div>
+
+           
             
        </div>
 
-       <div class="row">
-        <div class="col-12">
-
-        </div>
-       </div>
+      
 
     </div>
 </section>
 	
+@endsection
+
+@section('footer_js')
+    
+
+<script>
+
+    function selectCandidate(data,voter)
+    {
+        var name = document.getElementById(data).value;
+        var selectedName = document.getElementById('SelectedName').innerHTML = name;
+        var candidateID = document.getElementById(voter).value;
+        document.getElementById('getcandidateID').value = candidateID;
+        document.getElementById('FORMSUBMIT').style.display = "block";
+
+
+        
+    }
+
+</script>
+
+
 @endsection
