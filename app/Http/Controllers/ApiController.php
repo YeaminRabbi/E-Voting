26820a234f35151004180485f71fbf63f405255c
yeaminrabbi <yeaminrabbi308@gmail.com>
2022-07-28 17:04:23 +0600

    Api - User | Polls

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Auth;
+
+use App\User;
+use Carbon\Carbon;
+use App\Candidate;
+use App\VotingPortal;
+use App\DuplicateEmailUser;
+use App\Vote;
+
+use App\Http\Resources\UserResournce;
+use App\Http\Resources\PollResource;
+
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\File;
+use Intervention\Image\Facades\Image as Image;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+use Illuminate\Support\Facades\DB;
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Reader\Exception;
+use PhpOffice\PhpSpreadsheet\Writer\Xls;
+use PhpOffice\PhpSpreadsheet\IOFactory;
+
+
+class ApiController extends Controller
+{
+    function getUser()
+    {
+        return UserResournce::collection(User::all());
+    }
+
+   function getPolls()
+   {
+        return PollResource::collection(VotingPortal::all());
+   }
+}
