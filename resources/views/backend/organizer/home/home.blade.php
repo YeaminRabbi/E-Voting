6febda6d6454214d63d01d07a4b884b538f54e7a
YeaminRabbi <yeaminrabbi308@gmail.com>
2022-07-31 23:36:53 +0600

    dashboard

--- resources/views/backend/organizer/home/home.blade.php
+++ resources/views/backend/organizer/home/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('page_content')
     
-	<div class="card mt-3">
+	{{--  <div class="card mt-3">
         <div class="card-content">
             <div class="row row-group m-0">
                 <div class="col-12 col-lg-6 col-xl-3 border-light">
@@ -253,5 +253,5 @@
            </div>
          </div>
         </div><!--End Row-->
-    
+      --}}
 @endsection
